30c85dff69987e3516b3232aa4c4c25186e3afde
shikai liu <lsk569937453@gmail.com>
2025-06-20 16:47:41 +0800

    Remove unusuful code

--- rust-proxy/src/constants/common_constants.rs
+++ rust-proxy/src/constants/common_constants.rs
@@ -1,12 +1,5 @@
 pub const DEFAULT_ADMIN_PORT: i32 = 8870;
-pub const DENY_RESPONSE: &str = r#"{
-    "response_code": -1,
-    "response_object": "The request has been blocked by the Spire!"
-}"#;
-pub const NOT_FOUND: &str = r#"{
-    "response_code": -1,
-    "response_object": "The route could not be found in the Proxy!"
-}"#;
+
 pub const DEFAULT_FIXEDWINDOW_MAP_SIZE: i32 = 3;
 
 pub const TIMER_WAIT_SECONDS: u64 = 5;
